440b6d771ad87443f037d7e0f5bc1f0acf4a5e1f
MoVo01 <moritzvogel24@gmail.com>
2018-09-25 12:42:32 +0200

    fix Arc Reflection
    author = lukas
    add 2nd case for angle conditions

--- src/control/Raytracer.java
+++ src/control/Raytracer.java
@@ -117,18 +117,21 @@ public class Raytracer {
     public Double[] getCircleArcIntersection(CircleArc arc){
         Double[] factors = getCircleIntersection(arc.toCircle());
         Point S;
-        double intersecangle;
         if (factors[0] != null) {
             S = this.ray.getPoint(factors[0]);
-            // Winkel zwischen Start und SP
-            intersecangle = (new Vector(1,0).getDirAngle((new Line(arc.center, S)).toVector())) - arc.offsetangle;
-            //Außerhalb des Kreisbogens -> kein SP mit CircleArc
-            if (intersecangle < 0 || intersecangle > arc.arcangle) factors[0] = null;
+            Vector S_offset = new Vector(Math.cos(Math.toRadians(arc.offsetangle)), -Math.sin(Math.toRadians(arc.offsetangle)));
+            if (S_offset.getDirAngle((new Line(arc.center, S)).toVector()) > arc.arcangle) factors[0] = null;
+//            // Winkel zwischen Start und SP
+//            intersecangle = (new Vector(1,0).getDirAngle((new Line(arc.center, S)).toVector())) - arc.offsetangle;
+//            //Außerhalb des Kreisbogens -> kein SP mit CircleArc
+//            if (intersecangle < 0 || intersecangle > arc.arcangle) factors[0] = null;
         }
         if (factors[1] != null){
             S = this.ray.getPoint(factors[1]);
-            intersecangle = (new Vector(1,0).getDirAngle((new Line(arc.center, S)).toVector())) - arc.offsetangle;
-            if (intersecangle < 0 || intersecangle > arc.arcangle) factors[1] = null;
+            Vector S_offset = new Vector(Math.cos(Math.toRadians(arc.offsetangle)), -Math.sin(Math.toRadians(arc.offsetangle)));
+            if (S_offset.getDirAngle((new Line(arc.center, S)).toVector()) > arc.arcangle) factors[1] = null;
+//            intersecangle = (new Vector(1,0).getDirAngle((new Line(arc.center, S)).toVector())) - arc.offsetangle;
+//            if (intersecangle < 0 || intersecangle > arc.arcangle) factors[1] = null;
         }
         return factors;
     }
