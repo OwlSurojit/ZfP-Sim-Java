fd15b72bca6ef50dcca5710132d2dc79dcb1177c
Alchemyst42 <40919225+Alchemyst42@users.noreply.github.com>
2018-09-26 08:21:30 +0200

    Drag&Drop for Oval class

--- src/shapesBase/Oval.java
+++ src/shapesBase/Oval.java
@@ -16,7 +16,7 @@ public class Oval extends ShapeBase{
         if (p1.equals(p2)){
             throw new ValueException("P1 and P2 mustn't be identical.");
         }
-        else if (e <= (new Line(p1, p2)).length()){
+        else if (e <= p1.dist(p2)){
             throw new ValueException("E must be greater than distance of P1 and P2.");
         }
         else {
@@ -43,6 +43,7 @@ public class Oval extends ShapeBase{
     @Override
     public ArrayList<Binding> getDragPoints(){
         ArrayList<Binding> list = new ArrayList<Binding>();
+        list.add(new Binding(Point.center(p1, p2), this, bindType.OVAL_CENTER));
         list.add(new Binding(p1, this, bindType.OVAL_P1));
         list.add(new Binding(p2, this, bindType.OVAL_P2));
         return list;
@@ -50,6 +51,28 @@ public class Oval extends ShapeBase{
     
     @Override
     public void refactor(Binding bind, double nx, double ny){
-        
+        switch(bind.type){
+            case OVAL_CENTER:
+                Point c = Point.center(p1, p2);
+                double tx = (nx-c.x);
+                double ty = (ny-c.y);
+                p1 = new Point(p1.x + tx, p1.y + ty);
+                p2 = new Point(p2.x + tx, p2.y + ty);
+                break;
+            case OVAL_P1:
+                Point np1 = new Point(nx, ny);
+                if(e > np1.dist(p2)){
+                    p1 = np1;
+                }
+                break;
+            case OVAL_P2:
+                Point np2 = new Point(nx, ny);
+                if(e > np2.dist(p1)){
+                    p2 = np2;
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
