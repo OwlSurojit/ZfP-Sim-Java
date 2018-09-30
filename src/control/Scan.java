375a893989d85bed0784594d9e19ad77f09cc05c
Alchemyst42 <40919225+Alchemyst42@users.noreply.github.com>
2018-09-30 10:35:22 +0200

    Now using closest

--- src/control/Scan.java
+++ src/control/Scan.java
@@ -27,7 +27,7 @@ public class Scan {
         this.objects = shape_Arr;
         
         this.sender = sender;
-        sender.obj = objects[0];//closest(objects);
+        sender.obj = closest();
         this. numref = numref;
         this.velocity = velocity;
         this.delay = delay;
@@ -191,9 +191,9 @@ public class Scan {
         return points;
     }
 
-    private Object closest(Object[] objects) {
+    private Object closest() {
         return objects[0];
-        // TODO: nächstes Element ermitteln; prüfen, ob Abstand < 1, sonst null ausgeben (Heißt).
+        // TODO: nächstes Element ermitteln; prüfen, ob Abstand < 1, sonst null ausgeben.
     }
     
 }
